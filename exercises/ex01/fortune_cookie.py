c65b5150b9e4c07ef8e7428fb651af3b180d379f
Sophie Nichols <sophnich@live.unc.edu>
2021-02-05 00:47:29 -0500

    fortune cookie test

--- exercises/ex01/fortune_cookie.py
+++ exercises/ex01/fortune_cookie.py
@@ -1,6 +1,8 @@
 """Program that outputs one of at least four random, good fortunes."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+"""__author__ = "730314283" """
+
+
 
 # The randint function is imported from the random library so that
 # you are able to generate integers at random.
@@ -10,6 +12,20 @@ __author__ = "ENTER YOUR 9-DIGIT PID HERE"
 # For example, consider the function call expression: randint(1, 100)
 # It will evaluate to an int value >= 1 and <= 100. 
 from random import randint
-
-
 # Begin your solution here...
+
+
+
+print("Your fourtune cookie says...")
+from random import randint
+fortune: int = randint(1, 3)
+if fortune <= 2: 
+    if fortune < 2:
+        print("You will lose all your blueberry muffins")
+    else: 
+        print("You will adopt a rabbit and name it Lil Windex.")
+else: 
+    if fortune > 2:
+        print ("Your mom will invest all your college fund money in gamestop.")
+
+print("Now, go spread positive vibes!")
